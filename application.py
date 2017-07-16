2e469482f364ba450f3a52c773e3fa63710b3a86
alexcreamer <carebear@nili.ca>
2017-07-15 21:16:13 -0300

    Removed a line and moved some lines

--- application.py
+++ application.py
@@ -3,8 +3,6 @@ import bank
 conn = bank.Connector('localhost', user='user', passwd='password', db='init')
 
 print ("**************** print all persons in database****************")
-
-
 # perform query and get results
 result = conn.query('SELECT * FROM person')
 for item in result:
@@ -12,7 +10,6 @@ for item in result:
   print("Hi my name is {0.name} with {0.person_id}".format(person))
 
 print ("\n**************** print person that has id = 1 ****************")
-
 # retrieves the person SQL table that has id 1
 result = conn.query("select * from person where person_id = 1");
 first = list(result)[0]
